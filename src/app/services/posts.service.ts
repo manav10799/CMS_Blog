66d3dcfc60bc26cf293f292fea56f7900fed9ffa
Manav <manav10799@gmail.com>
2023-01-30 01:15:24 +0530

    Changed the Api Server for Local And Prod

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { AddPostModel, AddTags, PostsModel } from "./posts.model";
+import { environment } from "src/environments/environments";
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +11,21 @@ export class PostsService {
 
   constructor(private http: HttpClient) { }
 
+  apiUrl = environment.apiUrl;
   getPosts():Observable<any> {
-    return this.http.get('https://json-server-mauve.vercel.app/posts');
+    return this.http.get(`${this.apiUrl}/posts`);
   }
 
   addPosts(posts: AddPostModel):Observable<any> {
-    return this.http.post('https://json-server-mauve.vercel.app/posts', posts)
+    return this.http.post(`${this.apiUrl}/posts`, posts)
   }
 
   getIndividualPost(id:number):Observable<any> {
-    return this.http.get(`https://json-server-mauve.vercel.app/posts/${id}`);
+    return this.http.get(`${this.apiUrl}/posts/${id}`);
   }
 
   updateLikeCount(post:AddPostModel,id:number): Observable<any> {
-    return this.http.put(`https://json-server-mauve.vercel.app/posts/${id}`,post);
+    return this.http.put(`${this.apiUrl}/posts/${id}`,post);
   }
 
 }
